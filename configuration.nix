7e7a65696196e3fa703aa6c61cd94453eacccd12
Polen <polensky@pm.me>
2024-05-11 22:57:21 -0400

    smol change

--- configuration.nix
+++ configuration.nix
@@ -104,14 +104,6 @@
     QT_SCALE_FACTOR_ROUNDING_POLICY = "RoundPreferFloor";
   };
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
   # List services that you want to enable:
   security.rtkit.enable = true;
   services.pipewire = {
