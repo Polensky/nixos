ee94fba01faa713cf3a51af7516fdb162fcabd6d
Charles Sirois <charles@rumandcode.io>
2025-03-20 09:15:20 -0400

    main updated macbook config

--- flake.nix
+++ flake.nix
@@ -4,12 +4,16 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     nix-darwin.url = "github:LnL7/nix-darwin";
-    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
-		nixvim.url = "github:Polensky/nixvim";
-		sops-nix.url = "github:Mic92/sops-nix";
+    nixvim.url = "github:Polensky/nixvim";
+    sops-nix.url = "github:Mic92/sops-nix";
   };
 
-  outputs = {nixpkgs, nix-darwin, sops-nix ,...} @ inputs: {
+  outputs = {
+    nixpkgs,
+    nix-darwin,
+    sops-nix,
+    ...
+  } @ inputs: {
     nixosConfigurations = {
       default = nixpkgs.lib.nixosSystem {
         specialArgs = {inherit inputs;};
@@ -33,23 +37,25 @@
         modules = [
           "${nixpkgs}/nixos/modules/installer/sd-card/sd-image-aarch64-installer.nix"
           ./devices/pi/configuration.nix
-					sops-nix.nixosModules.sops
+          sops-nix.nixosModules.sops
           {
             sdImage.compressImage = false;
-						nixpkgs.overlays = [
+            nixpkgs.overlays = [
               (final: super: {
                 makeModulesClosure = x:
-                  super.makeModulesClosure (x // { allowMissing = true; });
+                  super.makeModulesClosure (x // {allowMissing = true;});
               })
             ];
           }
         ];
       };
-			darwinConfigurations."Charless-MacBook-Pro" = nix-darwin.lib.darwinSystem {
-				modules = [ ./devices/macbook/configuration.nix ];
-				specialArgs = { inherit inputs; };
-			};
+    };
+    darwinConfigurations = {
+      "Charless-MacBook-Pro" = nix-darwin.lib.darwinSystem {
+        modules = [./devices/macbook/configuration.nix];
+        specialArgs = {inherit inputs;};
+				system = "x86_64-darwin";
+      };
     };
   };
 }
-
