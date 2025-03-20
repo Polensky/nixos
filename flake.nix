106f2e7dec99c1aa55bffdd522264bd29e59b3ab
Charles Sirois <charles@rumandcode.io>
2025-03-20 14:08:49 -0400

    add m4 config with skhd dots

--- flake.nix
+++ flake.nix
@@ -43,5 +43,6 @@
   in {
     homeConfigurations."polen@xps13" = mkHomeConfig ./devices/xps13.nix "x86_64-linux";
     homeConfigurations."polen@pinephone" = mkHomeConfig ./devices/pinephone.nix "aarch64-linux";
+    homeConfigurations."charles@mbp-m4" = mkHomeConfig ./devices/mpb-m4.nix "aarch64-darwin";
   };
 }
