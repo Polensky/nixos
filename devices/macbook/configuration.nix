03eba9522d32855a8da05beddbeeed337634653d
Charles Sirois <charles@Mac.lan>
2025-04-11 11:04:22 -0400

    its stable

--- devices/macbook/configuration.nix
+++ devices/macbook/configuration.nix
@@ -1,4 +1,7 @@
-{pkgs, inputs, system, ...}: {
+{pkgs, inputs, system, config, ...}: 
+let
+	my-emacs = pkgs.emacsNativeComp;
+in{
   nixpkgs.config.allowUnfree = true;
 
   environment.systemPackages = with pkgs; [
@@ -12,6 +15,13 @@
 
 		# work tool
 		git
+		gnupg
+    (pass.withExtensions (exts: [exts.pass-otp]))
+
+		# emacs
+		my-emacs
+		fd
+		ripgrep
 
 		(writeShellScriptBin "drs" ''
       darwin-rebuild switch --flake ~/.config/nixos
@@ -25,12 +35,21 @@
 			cleanup = "zap";
 		};
 
+		brews = [
+		# doom emacs
+		 "fontconfig"
+		];
+
 		casks = [
 			# internet
 			"spotify"
+			"brave-browser"
 
 			# work
 			"slack"
+
+			# doom emacs
+			"font-symbols-only-nerd-font"
 		];
 	};
 
@@ -40,11 +59,17 @@
   # Create /etc/zshrc that loads the nix-darwin environment.
   programs.zsh.enable = true; # default shell on catalina
 	programs.direnv.enable = true;
+	programs.gnupg.agent.enable = true;
 
   services.yabai = {
     enable = true;
   };
   services.skhd.enable = true;
+
+services.emacs = {
+  enable = true;
+  package = my-emacs;
+};
 
   # Used for backwards compatibility, please read the changelog before changing.
   # $ darwin-rebuild changelog
@@ -61,6 +86,22 @@
 
 	system.defaults.menuExtraClock = {
 	  Show24Hour = true;
+	};
+
+	launchd.user.agents.remap-keys = {
+    serviceConfig = {
+      ProgramArguments = [
+        "/usr/bin/hidutil"
+        "property"
+        "--set"
+        ''{
+          "UserKeyMapping":[
+          {"HIDKeyboardModifierMappingSrc":0x700000039,"HIDKeyboardModifierMappingDst":0x7000000E7}
+          ]
+        }''
+      ];
+      RunAtLoad = true;
+    };
 	};
 
   # The platform the configuration will be used on.
