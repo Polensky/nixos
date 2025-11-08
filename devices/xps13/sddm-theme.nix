0f40f202b4e2bfb6c3eb209bc62fc53f95c87b69
Polen <polensky@pm.me>
2025-11-07 21:16:04 -0500

    sddm still broken (but functional)

--- devices/xps13/sddm-theme.nix
+++ devices/xps13/sddm-theme.nix
@@ -1,20 +1,19 @@
-{ pkgs }:
-let
+{pkgs}: let
   image = ./assets/a_forest_of_trees_with_fog.jpg;
 in
-pkgs.stdenv.mkDerivation {
-  name = "sddm-theme";
-  src = pkgs.fetchFromGitHub {
-    owner = "MarianArlt";
-    repo = "sddm-sugar-dark";
-    rev = "ceb2c455663429be03ba62d9f898c571650ef7fe";
-    sha256 = "0153z1kylbhc9d12nxy9vpn0spxgrhgy36wy37pk6ysq7akaqlvy";
-  };
-  installPhase = ''
-    mkdir -p $out
-    cp -R ./* $out/
-    cd $out/
-    rm Background.jpg
-    cp -r ${image} $out/Background.jpg
-   '';
-}
+  pkgs.stdenv.mkDerivation {
+    name = "sddm-theme";
+    src = pkgs.fetchFromGitHub {
+      owner = "MarianArlt";
+      repo = "sddm-sugar-dark";
+      rev = "ceb2c455663429be03ba62d9f898c571650ef7fe";
+      sha256 = "flOspjpYezPvGZ6b4R/Mr18N7N3JdytCSwwu6mf4owQ=";
+    };
+    installPhase = ''
+      mkdir -p $out
+      cp -R ./* $out/
+      cd $out/
+      rm Background.jpg
+      cp -r ${image} $out/Background.jpg
+    '';
+  }
