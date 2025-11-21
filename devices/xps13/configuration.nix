04505d3fd54b610813cc55e13eb63e10806061dd
Polen <polensky@pm.me>
2025-11-21 12:54:25 -0500

    avahi/hostname update

--- devices/xps13/configuration.nix
+++ devices/xps13/configuration.nix
@@ -1,6 +1,6 @@
 # Edit this configuration file to define what should be installed on
 # your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
+# and in the NixOS manual (accessible by running 'nixos-help').
 {
   config,
   pkgs,
@@ -24,7 +24,7 @@
 
   boot.kernelModules = ["msr"];
 
-  networking.hostName = "nixos"; # Define your hostname.
+  networking.hostName = "xps13"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
   nix = {
@@ -64,7 +64,7 @@
     theme = "${import ./sddm-theme.nix {inherit pkgs;}}";
   };
 
-  # Define a user account. Don't forget to set a password with ‘passwd’.
+  # Define a user account. Don't forget to set a password with 'passwd'.
   users.users.polen = {
     isNormalUser = true;
     description = "polen";
@@ -145,6 +145,14 @@
     enable = true;
     nssmdns4 = true;
     openFirewall = true;
+    publish = {
+      enable = true;
+      addresses = true;
+      domain = true;
+      hinfo = true;
+      userServices = true;
+      workstation = true;
+    };
   };
 
   # Enable the OpenSSH daemon.
@@ -158,7 +166,7 @@
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # on your system were taken. It's perfectly fine and recommended to leave
   # this value at the release version of the first install of this system.
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
